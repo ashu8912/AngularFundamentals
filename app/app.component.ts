aee49c91fcb3c3bf5de975f31b5f0bddba1f1785
ashu8912 <ashughildiyal5@gmail.com>
2019-01-09 11:26:24 +0530

    interpolations and expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,13 @@ import {Component} from '@angular/core';
   selector:'app-root',
   styleUrls:['app.component.scss'],
   template:`<div>
-  {{title}}</div>`
+  {{title}}{{numberOne+numberTwo}}{{isHappy?':)':':('}}</div>`
 })
 export class AppComponent{
   title:string;
+  numberOne:number=1;
+  numberTwo:number=2;
+  isHappy:boolean=false;
   constructor()
   {
     this.title="First Angular Component";
