5c7f0a9772d71c75a8dec98bb98e22113b206885
ashu8912 <ashughildiyal5@gmail.com>
2019-01-09 11:52:32 +0530

    event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,15 +5,21 @@ import {Component} from '@angular/core';
   template:`<div>
   <h1 [innerHTML]="title"></h1>
   <h1>{{title}}</h1>
+  <input type="text" [value]="name" (input)="handleInput($event)"/>
+  {{name}}
   {{numberOne+numberTwo}}{{isHappy?':)':':('}}</div>`
 })
 export class AppComponent{
   title:string;
+  name:String="";
   numberOne:number=1;
   numberTwo:number=2;
   isHappy:boolean=false;
   constructor()
   {
     this.title="First Angular Component";
+  }
+  handleInput(event:any){
+  this.name=event.target.value;
   }
 }
