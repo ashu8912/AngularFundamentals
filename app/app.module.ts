7ce72abb38a38762e382d5dd99bdfb2fb2079092
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 08:25:47 +0530

    added PassengerDashboard module as import in App module

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,10 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 @NgModule({
   declarations:[AppComponent],
-  imports:[BrowserModule,CommonModule,FormsModule],
-bootstrap:[AppComponent]
+  imports:[BrowserModule,CommonModule,FormsModule,PassengerDashboardModule],
+  bootstrap:[AppComponent]
 })
 export class AppModule{}
