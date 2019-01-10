f9aad3aa24edb34f3e1ff6277ac538e120d0cddc
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 08:27:35 +0530

    moved data from app to Passenger container

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,5 @@
 import {Component} from '@angular/core';
 
-interface Person{
-  id:number;
-  name:string;
-  checkedIn:boolean;
-  checkInDate?:number;
-}
-
-
-
 @Component({
   selector:'app-root',
   styleUrls:['app.component.scss'],
@@ -26,17 +17,7 @@ interface Person{
  </div>
   {{name}}
   {{numberOne+numberTwo}}{{isHappy?':)':':('}}</div>
-  <h3>Passengers</h3>
-  <ul>
-  <li *ngFor="let passenger of passengers;let i=index">
-  <span class="status" [ngClass]="{
-    'checked-in':passenger.checkedIn
-  }"></span>{{passenger.name}}
-  <h3>CheckInDate {{
-    passenger.checkInDate?(passenger.checkInDate | date:'yMMMMd'):'Not Checked In'}}</h3>
-  <p>{{passenger | json}}</p>
-  </li>
-  </ul>
+  <passenger-dashboard></passenger-dashboard>
 `
 
 })
@@ -46,26 +27,7 @@ export class AppComponent{
   numberOne:number=1;
   numberTwo:number=2;
   isHappy:boolean=false;
-  passengers:Person[]=[{
-    id:1,
-    name:"Ashu",
-    checkedIn:true,
-    checkInDate:1489129012902
-  },{
-    id:2,
-    name:"Todd",
-    checkedIn:true,
-    checkInDate:1432601212902
-  },{
-    id:1,
-    name:"Bravo",
-    checkedIn:false,
-    checkInDate:148299999902
-  },{
-    id:1,
-    name:"Kohli",
-    checkedIn:false
-  }]
+  
   constructor()
   {
     this.title="First Angular Component";
