00bc88a5c29f0006ee4ff967d9df213d13581e40
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 08:24:58 +0530

    created our first smart component

---
+++ app/passenger-dashboard/containers/passenger-dashboard.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import { Person } from '../models/passenger.interface';
+@Component({
+    selector:'passenger-dashboard',
+    styleUrls:['passenger-dashboard.component.scss'],
+    template:`
+    <div>
+    <h3>Passengers</h3>
+  <ul>
+  <li *ngFor="let passenger of passengers;let i=index">
+  <span class="status" [ngClass]="{
+    'checked-in':passenger.checkedIn
+  }"></span>{{passenger.name}}
+  <h3>CheckInDate {{
+    passenger.checkInDate?(passenger.checkInDate | date:'yMMMMd'):'Not Checked In'}}</h3>
+  <p>{{passenger | json}}</p>
+  </li>
+  </ul>
+    </div>`
+})
+export class PassengerDashboardComponent{
+    passengers:Person[]=[{
+        id:1,
+        name:"Ashu",
+        checkedIn:true,
+        checkInDate:1489129012902
+      },{
+        id:2,
+        name:"Todd",
+        checkedIn:true,
+        checkInDate:1432601212902
+      },{
+        id:1,
+        name:"Bravo",
+        checkedIn:false,
+        checkInDate:148299999902
+      },{
+        id:1,
+        name:"Kohli",
+        checkedIn:false
+      }]
+}
