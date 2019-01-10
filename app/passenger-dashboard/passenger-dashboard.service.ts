ebaacdf96c8aaf597c0871d2aeba541c96b142fd
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 15:23:36 +0530

    @Injectable to inject http service in our service

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,9 @@
 import {Person} from './models/passenger.interface';
+import {Http} from '@angular/http';
+import {Injectable} from '@angular/core';
+@Injectable()
 export class PassengerDashboardService{
-constructor(){}
+constructor(private http:Http){}
 getPassengers():Person[]{
     return [{
         id:1,
