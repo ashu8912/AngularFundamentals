d7f08460ef92ca477631586407ee5547ac77039c
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 15:22:52 +0530

    added HttpModule in imports

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import {HttpModule} from '@angular/http';
 import {PassengerDashboardComponent} from './containers/passenger-dashboard.component';
 import {PassengerDetailComponent} from './components/passenger-detail/passenger-detail.component';
 import {PassengerCountComponent} from './components/passenger-count/passenger-count.component';
@@ -8,7 +9,7 @@ import {PassengerDashboardService} from './passenger-dashboard.service';
     declarations:[PassengerDashboardComponent,
         PassengerDetailComponent,
         PassengerCountComponent],
-    imports:[CommonModule],
+    imports:[CommonModule,HttpModule],
     providers:[PassengerDashboardService],
     exports:[PassengerDashboardComponent]
 })
