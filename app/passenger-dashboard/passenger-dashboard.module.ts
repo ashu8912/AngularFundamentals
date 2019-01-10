2063f1efdfe055b4f85dce6cf0beea8bf80ada63
ashu8912 <ashughildiyal5@gmail.com>
2019-01-10 08:24:01 +0530

    created Passenger dashboard module

---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import {PassengerDashboardComponent} from './containers/passenger-dashboard.component';
+@NgModule({
+    declarations:[PassengerDashboardComponent],
+    imports:[CommonModule],
+    exports:[PassengerDashboardComponent]
+})
+export class PassengerDashboardModule{};
